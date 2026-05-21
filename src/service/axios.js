9f2956a9bdb7c184c8a378a5ade917c080c84aa8
Gideon Sowah <gideonsowah9@gmail.com>
2026-05-21 06:13:32 +0000

    update server url

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,3 +1,6 @@
 import axios from "axios";
-const prod = { local: "http://localhost:5000/api/", dev: "" };
-export const api = axios.create({ baseURL: prod.local });
+const prod = {
+  local: "http://localhost:5000/api/",
+  dev: "https://paystack-server-ckku.onrender.com/api/",
+};
+export const api = axios.create({ baseURL: prod.dev });
